d3f9178c6f55e18186dec0c7bece05d4fcc46992
keanpaderes <kean.paderes@gmail.com>
2016-07-11 18:38:08 +0800

    Finished up to update

---
+++ djnotes/notes/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.Index.as_view(), name='index'),
+    url(r'^edit/(?P<pk>\d+)/$', views.Update.as_view(), name="update"),
+]
--- djnotes/notes/views.py
+++ djnotes/notes/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import Http404
+
+#Added models
+from .models import Note
+from .forms import NoteForm
 
 # Create your views here.
+class Index(View):
+    template_name = 'notes/index.html'
+
+    def get_context_data(self):
+        context = {
+            'notes' : Note.objects.order_by('-id'),
+        }
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data()
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        form = NoteForm(request.POST)
+        context = self.get_context_data()
+        if form.is_valid():
+            form.save()
+        return render(request, self.template_name, context)
+
+class Update(View):
+    template_name = 'notes/update.html'
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        try:
+            note_id = kwargs.get('pk', 0)
+            note = Note.objects.get(id=note_id)
+        except Note.DoesNotExist:
+            raise Http404
+        else:
+            context['note'] = note
+            return context
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        print(self.template_name)
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        form = NoteForm(request.POST, instance=context.get('note'))
+        if form.is_valid():
+            form.save()
+        return render(request, self.template_name, context)
